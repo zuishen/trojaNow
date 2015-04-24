1c7abb49397bab0e263d8d05636499eb7f3fa8ef
Zui Shen <Jim@Zuis-MacBook-Pro.local>
2015-04-23 18:35:56 -0700

    make it better

--- webAPI/IndexServer.php
+++ webAPI/IndexServer.php
@@ -53,6 +53,7 @@
 		
 		
 		function loginAction() {
+			echo $_POST["user_account”];
 			if(isset($_POST["user_account"]) && isset($_POST["user_pwd"]) ) {
 				$account = $_POST["user_account"];
 				$passwd = $_POST["user_pwd"];
